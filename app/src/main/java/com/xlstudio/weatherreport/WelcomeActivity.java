ee82295c95b1b4bf0c121d707124a15968ea325f
superyxw <superyxw@163.com>
2016-03-24 17:32:51 +0800

    修改参数

--- app/src/main/java/com/xlstudio/weatherreport/WelcomeActivity.java
+++ app/src/main/java/com/xlstudio/weatherreport/WelcomeActivity.java
@@ -19,7 +19,7 @@ public class WelcomeActivity extends AppCompatActivity{
             @Override
             public void run() {
                 try {
-                    Thread.sleep(3000);
+                    Thread.sleep(2999);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
